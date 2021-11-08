bcb45adbfe20b4dc0a10205b9ffda9d3817b51d0
Nicolás Ibarra <nicoibarra@Nicolass-MacBook-Pro.local>
2021-11-08 17:19:32 +0100

    short description

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.get("/", (req, res) => {
       ratings: "50+",
       address: "8 Rue de Bretagne, 75003 Paris",
       delay: "10 - 20 Mins (Au plus tôt)",
-      description:
-        "Profitez de chaque plaisir de la vie quotidienne. Le Pain Quotidien propose des ingrédients simples et sains, du bon pain, des fruits et des légumes frais et de saison issus de l’agriculture biologique.",
+      description: "Profitez de chaque plaisir de la vie quotidienne.",
       picture: "https://f.roocdn.com/images/menus/17697/header-image.jpg",
       client_address: {
         coordinates: [2.36051359999999, 48.8737157],
@@ -38,7 +37,7 @@ app.get("/", (req, res) => {
             id: "1519055545-88",
             title: "Brunch authentique 1 personne",
             description:
-              "Assiette de jambon cuit, jambon fumeì, terrine, comté bio & camembert bio, salade jeunes pousses, oeuf poché bio, pain bio & confiture, 1 viennoiserie bio au choix, granola parfait bio, jus de fruits 33cl au choix",
+              "Assiette de jambon cuit, jambon fumeì, terrine, comté bio & camembert bio, salade jeunes pousses, oeuf poché bio, pain bio & confiture.",
             price: "25.00",
             picture:
               "https://f.roocdn.com/images/menu_items/1583350/item-image.jpg",
